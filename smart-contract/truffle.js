0f67927e4fd5209b4a9114132a1abe4bf6f776ee
Paul Zietsman <paulz@synthesis.co.za>
2019-04-20 17:14:44 +0200

    fixed truffle.js

--- smart-contract/truffle.js
+++ smart-contract/truffle.js
@@ -4,14 +4,8 @@ const HDWalletProvider = require("truffle-hdwallet-provider");
 module.exports = {
   networks: {
     xdai: {
-      provider: function() {
-            return new HDWalletProvider(
-           process.env.MNEMONIC,
-           "https://dai.poa.network")
-      },
-      network_id: 100,
-      gas: 5000000,
-      gasPrice: 1000000000
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://dai.poa.network"),
+      network_id: 100
     },
     development: {
       host: "127.0.0.1",
