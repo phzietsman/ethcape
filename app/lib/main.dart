beb8830346943c5258b1cf88e3fc0125d076e121
Paul Zietsman <paulz@synthesis.co.za>
2019-04-20 10:46:06 +0200

    basic message processing

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -67,83 +67,110 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  String _counter = '';
+  String messageReceived = '';
+  TextEditingController phoneNumberController = TextEditingController();
+  TextEditingController amountController = TextEditingController();
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    receiver.onSmsReceived.listen((SmsMessage msg) =>
+    {
 
-    receiver.onSmsReceived.listen((SmsMessage msg) => {
-
-        setState(() {
+    setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter = msg.body;
+      var words = msg.body.split(' ');
+      if(words.length != 4) {
+        messageReceived = "Wrong message received";
+      }else{
+        messageReceived = "Sending " + words[1] + " XDAI to " + words[3];
+      }
+
     })
 
     });
 
-//    setState(() {
-//      // This call to setState tells the Flutter framework that something has
-//      // changed in this State, which causes it to rerun the build method below
-//      // so that the display can reflect the updated values. If we changed
-//      // _counter without calling setState(), then the build method would not be
-//      // called again, and so nothing would appear to happen.
-//      _counter+= '';
-//    });
+  }
+
+  void sendMoney(BuildContext context) {
+    // First make sure there is some information in the form.
+    // A dog needs a name, but may be location independent,
+    // so we'll only abandon the save if there's no name.
+    if (phoneNumberController.text.isEmpty) {
+      print('');
+    } else {
+
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('EthGlobal'),
+        backgroundColor: Colors.grey,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      body: Container(
+        color: Colors.grey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 8.0,
+            horizontal: 32.0,
+          ),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: TextField(
+                  // Tell your textfield which controller it owns
+                    controller: phoneNumberController,
+                    // Every single time the text changes in a
+                    // TextField, this onChanged callback is called
+                    // and it passes in the value.
+                    //
+                    // Set the text of your controller to
+                    // to the next value.
+                    onChanged: (v) => phoneNumberController.text = v,
+                    decoration: InputDecoration(
+                      labelText: 'Recipient Cellnumber',
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: TextField(
+                    controller: amountController,
+                    onChanged: (v) => amountController.text = v,
+                    decoration: InputDecoration(
+                      labelText: "Amount",
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Builder(
+                  builder: (context) {
+                    return RaisedButton(
+                      onPressed: () => sendMoney(context),
+                      color: Colors.lightBlueAccent,
+                      child: Text('Send Money'),
+                    );
+                  },
+                ),
+              ),
+              Text(
+                '$messageReceived',
+                style: Theme.of(context).textTheme.body1,
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
 }
+
 
